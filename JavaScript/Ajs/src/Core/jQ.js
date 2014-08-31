c8c495c60a24fd70fdfbb9f17c94ba0483f615a3
Peterinor <tangyutianbulao@qq.com>
2014-09-01 00:15:28 +0800

    jQ

--- JavaScript/Ajs/src/Core/jQ.js
+++ JavaScript/Ajs/src/Core/jQ.js
@@ -13,146 +13,164 @@ export, manipulation, offset, queue, serialize, support, traversing, ajax
 ...
 */
 
-[Window, Document].invoke('implement', {
-    // DOM Load
-    ready: function(fn) {
-        window.addEvent('domready', fn);
-        return this;
-    }
-});
-
-Window.implement({
-    load: function(fn) {
-        return this.addEvent('load', fn);
-    },
-
-    unload: function(fn) {
-        return this.addEvent('unload', fn);
-    },
-
-    resize: function(fn) {
-        return this.addEvent('resize', fn);
-    },
-
-    scroll: function(fn) {
-        return this.addEvent('scroll', fn);
-    }
-});
-
 /*****   EVENTS   *****/
-[Element, Document, Window].invoke('implement', {
+(function() {
+    [Element, Document, Window].invoke('implement', {
 
-    bind: function(type, fn) {
-        type.split(' ').each(function(event) { // accepts multiple event types!
-            this.addEvent(event, fn);
-        }, this);
-        return this;
-    },
+        bind: function(type, fn) {
+            type.split(' ').each(function(event) { // accepts multiple event types!
+                this.addEvent(event, fn);
+            }, this);
+            return this;
+        },
 
-    unbind: function(type, fn) {
-        return this.removeEvent(type, fn);
-    },
+        unbind: function(type, fn) {
+            return this.removeEvent(type, fn);
+        },
 
-    one: function(type, fn) {
-        // TODO: Make this cleaner. Looks like a hack now.
-        var removeOne = function() {
-            this.removeEvent(type, fn).removeEvent(type, removeOne);
+        one: function(type, fn) {
+            // TODO: Make this cleaner. Looks like a hack now.
+            var removeOne = function() {
+                this.removeEvent(type, fn).removeEvent(type, removeOne);
+            }
+            return this.addEvent(type, fn).addEvent(type, removeOne);
+        },
+
+        trigger: function(type, args) {
+            return this.fireEvent(type, args);
+        },
+
+        triggerHandler: function() {
+
+        },
+
+        //TODO: to adjust to same as jquery on
+        on: function(type, fn) {
+            return this.addEvent(type, fn);
+        }.overloadSetter(),
+
+        off: function(type, fn) {
+            return this.removeEvent(type, fn);
+        }.overloadSetter(),
+
+        delegate: function() {
+
+        },
+
+        undelegate: function() {
+
         }
-        return this.addEvent(type, fn).addEvent(type, removeOne);
-    },
 
-    trigger: function(type, args) {
-        return this.fireEvent(type, args);
-    },
-    
-    triggerHandler:function(){
-
-    },
-
-    hover: function(fnOver, fnOut) {
-        return this.addEvents({
-            'mouseenter': fnOver,
-            'mouseleave': fnOut
-        });
-    },
-
-    //TODO: to adjust to same as jquery on
-    on: function(type, fn) {
-        return this.addEvent(type, fn);
-    }.overloadSetter(),
-
-    off: function(type, fn) {
-        return this.removeEvent(type, fn);
-    }.overloadSetter(),
-
-    delegate: function(){
-
-    },
-
-    undelegate: function(){
-
-    }
-
-});
-
-
-(function(types) {
-    var methods = {};
-    types.each(function(name) {
-        methods[name] = function(fn) {
-            var un_name = name.replace('_', '');
-            return fn ? this.addEvent(un_name, fn) : this.fireEvent(un_name);
-        };
     });
 
-    [Element, Document, Window].invoke('implement', methods);
+    [Window, Document].invoke('implement', {
+        // DOM Load
+        ready: function(fn) {
+            window.addEvent('domready', fn);
+            return this;
+        },
 
-})(['_blur', 'change', 'click', '_click', 'dblclick', 'error', '_focus', 'keydown', 'keypress', 'keyup', 'mousedown', 'mousemove', 'mouseout', 'mouseover', 'mouseup', '_select', '_submit', 'unload']);
+        load: function(fn) {
+            return this.addEvent('load', fn);
+        },
 
-
-//For jQuery API compact
-Element.implement({
-
-    removeData: function(prop) {
-        return this.eliminate(prop);
-    },
-
-    data: function(prop, value) {
-        return value ? this.store(prop, value) : this.retrieve(prop);
-    }
-});
-
-//For jQuery API compact
-Element.implement({
-    css: function(property, value) {
-        switch (typeOf(property)) {
-            case 'object':
-                this.setStyles(property);
-                break;
-            case 'string':
-                if (value) this.setStyle(property, value)
-                else return this.getStyle(property);
+        unload: function(fn) {
+            return this.addEvent('unload', fn);
         }
-        return this;
-    }
-});
+    });
 
-// Save a reference to some core methods
-core_push = Array.prototype.push,
-core_slice = Array.prototype.slice,
-core_indexOf = Array.prototype.indexOf,
-core_toString = Object.prototype.toString,
-core_hasOwn = Object.prototype.hasOwnProperty,
-core_trim = String.prototype.trim;
+    (function(types) {
+        var methods = {};
+        types.each(function(name) {
+            // methods[name] = function(fn) {
+            //     var un_name = name.replace('_', '');
+            //     return fn ? this.addEvent(un_name, fn) : this.fireEvent(un_name);
+            // };
+            methods[name] = function(data, fn) {
+                if (fn == null) {
+                    fn = data;
+                    data = null;
+                }
+
+                return arguments.length > 0 ?
+                    this.on(name, null, data, fn) :
+                    this.trigger(name);
+            };
+        });
+
+        [Element, Document, Window].invoke('implement', methods);
+
+        // })(['error',
+        //     'keydown', 'keypress', 'keyup', '_focusout', '_focusin',
+        //     'click', 'dblclick', 'mousedown', 'mouseup', 'mouseenter', 'mouseleave', 'mousemove', 'mouseover', 'mouseout',
+        //     '_blur', 'change', '_focus', '_select', '_submit',
+        //     'unload'
+        // ]);
+    })(("blur focus focusin focusout resize click dblclick " +
+        "mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave " +
+        "change select submit keydown keypress keyup error contextmenu").split(" "));
+    // load scroll unload
 
 
-var $ = this.$;
+    Window.implement({
 
-// Map over the $ in case of overwrite
+        resize: function(fn) {
+            return this.addEvent('resize', fn);
+        },
+
+        scroll: function(fn) {
+            return this.addEvent('scroll', fn);
+        }
+    });
+
+    [Element, Document, Window].invoke('implement', {
+        hover: function(fnOver, fnOut) {
+            return this.addEvents({
+                'mouseenter': fnOver,
+                'mouseleave': fnOut
+            });
+        }
+    });
+})();
+
+
+/*****   Data   *****/
+(function() {
+    Element.implement({
+
+        removeData: function(prop) {
+            return this.eliminate(prop);
+        },
+
+        data: function(prop, value) {
+            return value ? this.store(prop, value) : this.retrieve(prop);
+        }
+    });
+
+})();
+
+/*****   CSS   *****/
+(function() {
+    Element.implement({
+        css: function(property, value) {
+            switch (typeOf(property)) {
+                case 'object':
+                    this.setStyles(property);
+                    break;
+                case 'string':
+                    if (value) this.setStyle(property, value)
+                    else return this.getStyle(property);
+            }
+            return this;
+        }
+    });
+})();
+
+
+/*****   jQuery Export  *****/
 var _$$ = window.jQuery;
 var _$ = window.$;
-
-window.$ = $;
+window.jQuery = window.$ = this.$;
 
 this.$.extend({
     noConflict: function(deep) {
@@ -160,156 +178,220 @@ this.$.extend({
             window.$ = _$;
         }
 
-        if (deep && window.jQuery === $) {
+        if (deep && window.jQuery === this.$) {
             window.jQuery = _$$;
         }
 
-        return $;
-    },
+        return this.$;
+    }
+});
 
-    proxy:function(){
 
-    },
+// Save a reference to some core methods
+var core_push = Array.prototype.push,
+    core_slice = Array.prototype.slice,
+    core_indexOf = Array.prototype.indexOf,
+    core_toString = Object.prototype.toString,
+    core_hasOwn = Object.prototype.hasOwnProperty,
+    core_trim = String.prototype.trim;
 
-    each: function() {
+(function($) {
 
-    },
+    $.extend({
 
-    isFunction: function(obj) {
-        return $.type(obj) === "function";
-    },
+        proxy: function(fn, ctx) {
+            if ($.type(ctx) == 'string') {
+                fn = fn[ctx];
+                arguments[1] = ctx = fn;
+            }
+            Function.prototype.bind.apply(fn, Array.from(arguments).slice(1));
+        },
 
-    isArray: Array.isArray || function(obj) {
-        return $.type(obj) === "array";
-    },
+        each: function(obj, callback) {
+            var name,
+                i = 0,
+                length = obj.length,
+                isObj = length === undefined || $.isFunction(obj);
 
-    isWindow: function(obj) {
-        return obj != null && obj == obj.window;
-    },
 
-    isNumeric: function(obj) {
-        return !isNaN(parseFloat(obj)) && isFinite(obj);
-    },
+            if (isObj) {
+                for (name in obj) {
+                    if (callback.call(obj[name], name, obj[name]) === false) {
+                        break;
+                    }
+                }
+            } else {
+                for (; i < length;) {
+                    if (callback.call(obj[i], i, obj[i++]) === false) {
+                        break;
+                    }
+                }
+            }
+            return obj;
+        },
 
-    type: function(obj) {
-        return Ajs.typeOf(obj);
-    },
+        isFunction: function(obj) {
+            return $.type(obj) === "function";
+        },
 
-    isEmptyObject: function(obj) {
-        var name;
-        for (name in obj) {
-            return false;
+        isArray: Array.isArray || function(obj) {
+            return $.type(obj) === "array";
+        },
+
+        isWindow: function(obj) {
+            return obj != null && obj == obj.window;
+        },
+
+        isNumeric: function(obj) {
+            return !isNaN(parseFloat(obj)) && isFinite(obj);
+        },
+
+        type: Ajs.typeOf,
+
+        isPlainObject: function(obj) {
+            // Must be an Object.
+            // Because of IE, we also have to check the presence of the constructor property.
+            // Make sure that DOM nodes and window objects don't pass through, as well
+            if (!obj || $.type(obj) !== "object" || obj.nodeType || $.isWindow(obj)) {
+                return false;
+            }
+
+            try {
+                // Not own constructor property must be Object
+                if (obj.constructor && !core_hasOwn.call(obj, "constructor") && !core_hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
+                    return false;
+                }
+            } catch (e) {
+                // IE8,9 Will throw exceptions on certain host objects #9897
+                return false;
+            }
+
+            // Own properties are enumerated firstly, so to speed up,
+            // if last one is own, then all properties are own.
+
+            var key;
+            for (key in obj) {}
+
+            return key === undefined || core_hasOwn.call(obj, key);
+        },
+        isEmptyObject: function(obj) {
+            var name;
+            for (name in obj) {
+                return false;
+            }
+            return true;
+        },
+
+        error: function(msg) {
+            throw new Error(msg);
+        },
+
+        trim: function(v) {
+            return String(v).trim();
         }
-        return true;
-    },
-
-    error: function(msg) {
-        throw new Error(msg);
-    },
-
-    trim: function(v) {
-        return String(v).trim();
-    }
-});
+    });
 
 
-this.$.implement({
-    // The number of elements contained in the matched element set
-    size: function() {
-        return this.length;
-    },
+    $.implement({
+        // The number of elements contained in the matched element set
+        size: function() {
+            return this.length;
+        },
 
-    toArray: function() {
-        // return Array.from(this);
-    },
+        toArray: function() {
+            // return Array.from(this);
+        },
 
-    // Get the Nth element in the matched element set OR
-    // Get the whole matched element set as a clean array
-    get: function(num) {
-        return num == null ?
-            this.toArray() :
-            (num < 0 ? this[this.length + num] : this[num]);
-    },
+        // Get the Nth element in the matched element set OR
+        // Get the whole matched element set as a clean array
+        get: function(num) {
+            return num == null ?
+                this.toArray() :
+                (num < 0 ? this[this.length + num] : this[num]);
+        },
 
-    // // Take an array of elements and push it onto the stack
-    // // (returning the new matched element set)
-    // pushStack: function(elems, name, selector) {
+        // // Take an array of elements and push it onto the stack
+        // // (returning the new matched element set)
+        // pushStack: function(elems, name, selector) {
 
-    //     // Build a new jQuery matched element set
-    //     var ret = jQuery.merge(this.constructor(), elems);
+        //     // Build a new jQuery matched element set
+        //     var ret = jQuery.merge(this.constructor(), elems);
 
-    //     // Add the old object onto the stack (as a reference)
-    //     ret.prevObject = this;
+        //     // Add the old object onto the stack (as a reference)
+        //     ret.prevObject = this;
 
-    //     ret.context = this.context;
+        //     ret.context = this.context;
 
-    //     if (name === "find") {
-    //         ret.selector = this.selector + (this.selector ? " " : "") + selector;
-    //     } else if (name) {
-    //         ret.selector = this.selector + "." + name + "(" + selector + ")";
-    //     }
+        //     if (name === "find") {
+        //         ret.selector = this.selector + (this.selector ? " " : "") + selector;
+        //     } else if (name) {
+        //         ret.selector = this.selector + "." + name + "(" + selector + ")";
+        //     }
 
-    //     // Return the newly-formed element set
-    //     return ret;
-    // },
+        //     // Return the newly-formed element set
+        //     return ret;
+        // },
 
-    // // Execute a callback for every element in the matched set.
-    // // (You can seed the arguments with an array of args, but this is
-    // // only used internally.)
-    // each: function(callback, args) {
-    //     return jQuery.each(this, callback, args);
-    // },
+        // // Execute a callback for every element in the matched set.
+        // // (You can seed the arguments with an array of args, but this is
+        // // only used internally.)
+        // each: function(callback, args) {
+        //     return jQuery.each(this, callback, args);
+        // },
 
-    // ready: function(fn) {
-    //     // Add the callback
-    //     jQuery.ready.promise().done(fn);
+        // ready: function(fn) {
+        //     // Add the callback
+        //     jQuery.ready.promise().done(fn);
 
-    //     return this;
-    // },
+        //     return this;
+        // },
 
-    eq: function(i) {
-        return $(this.get(i));
-    },
+        eq: function(i) {
+            return $(this.get(i));
+        },
 
-    first: function() {
-        return this.eq(0);
-    },
+        first: function() {
+            return this.eq(0);
+        },
 
-    last: function() {
-        return this.eq(-1);
-    }
+        last: function() {
+            return this.eq(-1);
+        }
 
-    // map: function(callback) {
-    //     return this.pushStack(jQuery.map(this, function(elem, i) {
-    //         return callback.call(elem, i, elem);
-    //     }));
-    // },
+        // map: function(callback) {
+        //     return this.pushStack(jQuery.map(this, function(elem, i) {
+        //         return callback.call(elem, i, elem);
+        //     }));
+        // },
 
-    // end: function() {
-    //     return this.prevObject || this.constructor(null);
-    // },   
-    // // // For internal use only.
-    // // // Behaves like an Array's method, not like a jQuery method.
-    // // push: core_push,
-    // // sort: [].sort,
-    // // splice: [].splice
-});
+        // end: function() {
+        //     return this.prevObject || this.constructor(null);
+        // },   
+        // // // For internal use only.
+        // // // Behaves like an Array's method, not like a jQuery method.
+        // // push: core_push,
+        // // sort: [].sort,
+        // // splice: [].splice
+    });
+})(this.$);
 
 
-this.$.implement({
-    find: function(selector, util) {
-        var t = $();
-        t.selector = this.selector + ' ' + selector;
-        for (var i = 0, l = this.length; i < l; i++) {
-            // console.log(Array.from($(selector, this[i])));
-            // t.push.apply(t, Array.from($(selector, this[i])));
-            // t.push(Array.from($(selector, this[i])));
-            Array.prototype.push.apply(t, Array.from($(selector, this[i])));
-            // t.__push($(selector, this[i]));
-        };
-        return t;
-    }
-})
+(function($) {
+    $.implement({
+        find: function(selector, util) {
+            var t = $();
+            t.selector = this.selector + ' ' + selector;
+            for (var i = 0, l = this.length; i < l; i++) {
+                // console.log(Array.from($(selector, this[i])));
+                // t.push.apply(t, Array.from($(selector, this[i])));
+                // t.push(Array.from($(selector, this[i])));
+                Array.prototype.push.apply(t, Array.from($(selector, this[i])));
+                // t.__push($(selector, this[i]));
+            };
+            return t;
+        }
+    });
+})($);
 
 
 
--- JavaScript/Ajs/test/unit/Core.jQ.js
+++ JavaScript/Ajs/test/unit/Core.jQ.js
@@ -1,6 +1,117 @@
+// test("jQuery Compactable API", function() {
+//     ok(jQuery, "jQuery");
+//     ok(jQuery == $);
+//     ok(jQuery == Ajs.$);
+// });
 
-test("jQuery API", function(){
+test("Utilities", function() {
+    var func = function() {};
+    ok($.type("string") == 'string', '$.type("string")');
 
+    ok($.type(func) == 'function', '$.type(func)');
+    ok($.isFunction(func), '$.isFunction(func)');
+
+    ok($.type([]) == 'array', '$.type([])');
+    ok($.isArray([]), '$.isArray([])');
+    ok(!$.isArray({}), '$.isArray({}})');
+
+    ok($.isNumeric(2), '$.isNumeric(2)');
+    ok($.isNumeric("2"), '$.isNumeric("2")');
+    ok(!$.isNumeric("A2"), '$.isNumeric("A2")');
+
+    ok($.isWindow(window), '$.isWindow(window)');
+    ok(!$.isWindow(document), '$.isWindow(document)');
+
+    ok($.isEmptyObject({}), '$.isEmptyObject({})');
+    ok(!$.isEmptyObject({
+        a: 1
+    }), '$.isEmptyObject({a : 1})');
+
+    ok($.isPlainObject({
+        a: 1
+    }), '$.isPlainObject({a : 1})');
+    ok($.isPlainObject(new Object({
+        a: 1
+    })), '$.isPlainObject(new Object({a : 1}))');
+    ok(!$.isPlainObject(document), '$.isPlainObject(document)');
+
+});
+
+
+test("Utilities/$.each(Object,Function)", function() {
+    // expect(14);
+    $.each([0, 1, 2], function(i, n) {
+        equal(i, n, "Check array iteration");
+    });
+
+    $.each([5, 6, 7], function(i, n) {
+        equal(i, n - 5, "Check array iteration");
+    });
+
+    $.each({
+        name: "name",
+        lang: "lang"
+    }, function(i, n) {
+        equal(i, n, "Check object iteration");
+    });
+
+    var total = 0;
+    $.each([1, 2, 3], function(i, v) {
+        total += v;
+    });
+    equal(total, 6, "Looping over an array");
+    total = 0;
+    $.each([1, 2, 3], function(i, v) {
+        total += v;
+        if (i == 1) {
+            return false;
+        }
+    });
+    equal(total, 3, "Looping over an array, with break");
+    total = 0;
+    $.each({
+        "a": 1,
+        "b": 2,
+        "c": 3
+    }, function(i, v) {
+        total += v;
+    });
+    equal(total, 6, "Looping over an object");
+    total = 0;
+    $.each({
+        "a": 3,
+        "b": 3,
+        "c": 3
+    }, function(i, v) {
+        total += v;
+        return false;
+    });
+    equal(total, 3, "Looping over an object, with break");
+
+    var f = function() {};
+    f.foo = "bar";
+    $.each(f, function(i) {
+        f[i] = "baz";
+    });
+    equal("baz", f.foo, "Loop over a function");
+
+    // var stylesheet_count = 0;
+    // $.each(document.styleSheets, function(i) {
+    //     stylesheet_count++;
+    // });
+    // equal(stylesheet_count, 2, "should not throw an error in IE while looping over document.styleSheets and return proper amount");
+});
+
+
+test("Traversing", function() {
     ok($('div.find-test').find('p').length == 3, '$().find');
 
 });
+
+// test('$.noConflict', function() {
+
+//     var $ = Ajs.$.noConflict();
+
+//     ok($ != Ajs.$, 'Ajs.$/jQuery .noConflict()');
+
+// });
