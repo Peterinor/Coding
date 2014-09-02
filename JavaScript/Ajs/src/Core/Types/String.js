0bdfa0ce1df9ae573615a41ae70f3bba9a625024
Peterinor <tangyutianbulao@qq.com>
2014-09-02 22:40:18 +0800

    trim bug fixed for lt ie8 (charCode \uFEFF & \xA0)

--- JavaScript/Ajs/src/Core/Types/String.js
+++ JavaScript/Ajs/src/Core/Types/String.js
@@ -11,69 +11,73 @@ provides: [String]
 
 ...
 */
+var core_trim = String.prototype.trim;
+var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
 
 String.implement({
 
 	//<!ES6>
-	contains: function(string, index){
+	contains: function(string, index) {
 		return (index ? String(this).slice(index) : String(this)).indexOf(string) > -1;
 	},
 	//</!ES6>
 
-	test: function(regex, params){
+	test: function(regex, params) {
 		return ((typeOf(regex) == 'regexp') ? regex : new RegExp('' + regex, params)).test(this);
 	},
 
-	trim: function(){
-		return String(this).replace(/^\s+|\s+$/g, '');
+	trim: core_trim && !core_trim.call("\uFEFF\xA0") ? function() {
+		String(this).trim();
+	} : function() {
+		return String(this).replace(rtrim, '');
 	},
 
-	clean: function(){
+	clean: function() {
 		return String(this).replace(/\s+/g, ' ').trim();
 	},
 
-	camelCase: function(){
-		return String(this).replace(/-\D/g, function(match){
+	camelCase: function() {
+		return String(this).replace(/-\D/g, function(match) {
 			return match.charAt(1).toUpperCase();
 		});
 	},
 
-	hyphenate: function(){
-		return String(this).replace(/[A-Z]/g, function(match){
+	hyphenate: function() {
+		return String(this).replace(/[A-Z]/g, function(match) {
 			return ('-' + match.charAt(0).toLowerCase());
 		});
 	},
 
-	capitalize: function(){
-		return String(this).replace(/\b[a-z]/g, function(match){
+	capitalize: function() {
+		return String(this).replace(/\b[a-z]/g, function(match) {
 			return match.toUpperCase();
 		});
 	},
 
-	escapeRegExp: function(){
+	escapeRegExp: function() {
 		return String(this).replace(/([-.*+?^${}()|[\]\/\\])/g, '\\$1');
 	},
 
-	toInt: function(base){
+	toInt: function(base) {
 		return parseInt(this, base || 10);
 	},
 
-	toFloat: function(){
+	toFloat: function() {
 		return parseFloat(this);
 	},
 
-	hexToRgb: function(array){
+	hexToRgb: function(array) {
 		var hex = String(this).match(/^#?(\w{1,2})(\w{1,2})(\w{1,2})$/);
 		return (hex) ? hex.slice(1).hexToRgb(array) : null;
 	},
 
-	rgbToHex: function(array){
+	rgbToHex: function(array) {
 		var rgb = String(this).match(/\d{1,3}/g);
 		return (rgb) ? rgb.rgbToHex(array) : null;
 	},
 
-	substitute: function(object, regexp){
-		return String(this).replace(regexp || (/\\?\{([^{}]+)\}/g), function(match, name){
+	substitute: function(object, regexp) {
+		return String(this).replace(regexp || (/\\?\{([^{}]+)\}/g), function(match, name) {
 			if (match.charAt(0) == '\\') return match.slice(1);
 			return (object[name] != null) ? object[name] : '';
 		});
