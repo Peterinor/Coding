4a7c2c0289b0b79bcb257b415086ba233033da83
Peterinor <tangyutianbulao@qq.com>
2014-09-04 23:50:06 +0800

    Afx

---
+++ JavaScript/Ajs/src/More/Afx/Afx.Model.js
@@ -0,0 +1,30 @@
+/*
+---
+
+name: Afx.Model
+
+description: One of the most important items in MooTools. Contains the dollar function, the dollars function, and an handful of cross-browser, time-saver methods to let you easily work with HTML Elements.
+
+requires: [Array, Function, Number, String, Object, Number]
+provides: [Afx.Model]
+
+...
+*/
+
+
+Afx.Model = new Class({
+    initialize: function(o) {
+        console.log('model' + o);
+    }
+});
+
+
+Afx.User = new Class({
+
+    Extends: Afx.Model,
+
+    initialize: function(o) {
+        console.log('user' + o);
+        this.super(o);
+    }
+})
---
+++ JavaScript/Ajs/src/More/Afx/Afx.js
@@ -0,0 +1,18 @@
+/*
+---
+
+name: Afx
+
+description: One of the most important items in MooTools. Contains the dollar function, the dollars function, and an handful of cross-browser, time-saver methods to let you easily work with HTML Elements.
+
+requires: [Array, Function, Number, String, Object, Number]
+provides: [Afx]
+
+...
+*/
+
+var Afx = this.Afx = new Class({
+
+});
+
+Afx.Events = this.Events;
