0067c7c354388bd7f78bbab12c61c90b9747b252
Yu.Tang <Yu.Tang@hotmail.com>
2014-12-22 16:53:12 +0800

    Xcrc_gen

---
+++ CC++/crc/crc4net/xcrc_gen/CrcGen.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace xcrc_gen
+{
+    partial class CrcGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxInput = new System.Windows.Forms.TextBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBxResult = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBxInput
+            // 
+            this.txtBxInput.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxInput.Location = new System.Drawing.Point(17, 57);
+            this.txtBxInput.Multiline = true;
+            this.txtBxInput.Name = "txtBxInput";
+            this.txtBxInput.Size = new System.Drawing.Size(375, 97);
+            this.txtBxInput.TabIndex = 0;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGen.Location = new System.Drawing.Point(415, 55);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(117, 36);
+            this.btnGen.TabIndex = 1;
+            this.btnGen.Text = "generator";
+            this.btnGen.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input String:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Result:";
+            // 
+            // txtBxResult
+            // 
+            this.txtBxResult.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxResult.Location = new System.Drawing.Point(17, 201);
+            this.txtBxResult.Name = "txtBxResult";
+            this.txtBxResult.Size = new System.Drawing.Size(252, 29);
+            this.txtBxResult.TabIndex = 3;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(275, 201);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(117, 29);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(477, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "by Yu.Tang";
+            // 
+            // CrcGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 242);
+            this.Controls.Add(this.txtBxResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.txtBxInput);
+            this.Name = "CrcGen";
+            this.Text = "FMC CRC Generator of XiamenAir";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxInput;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBxResult;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
